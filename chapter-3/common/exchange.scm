a3c71d297426baf06164751dd740e09e99d4948b
DesmondoRay <desmondorayy@gmail.com>
2018-07-10 19:14:01 +0800

    update common files

---
+++ chapter-3/common/exchange.scm
@@ -0,0 +1,9 @@
+;;; exchage.scm
+;;; page 214
+
+(define (exchange account1 account2)
+  (let ((difference (- (account1 'balance)
+					   (account2 'balance))))
+	((account1 'withdraw) difference)
+	((account2 'deposit) difference)))
+					
