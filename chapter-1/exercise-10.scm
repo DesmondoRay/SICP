bf805a6addb965511494cd3fa8588f32dd1b8104
DesmondoRay <desmondorayy@gmail.com>
2018-07-12 02:23:28 +0800

    update exercise 10

--- chapter-1/exercise-10.scm
+++ chapter-1/exercise-10.scm
@@ -7,6 +7,15 @@
         (else (A (- x 1)
                  (A x (- y 1))))))
 
-(A 1 10)
-(A 2 4)
-(A 3 3)
+(newline)
+(display (A 1 10))
+(newline)
+(display (A 2 2))
+(newline)
+(display (A 3 3))
+
+;; 给出下列f, g, h, k 的数学定义
+(define (f n) (A 0 n))  ; --> 2 * n
+(define (g n) (A 1 n))  ; --> 2 ^ n
+(define (h n) (A 2 n))  ; --> 2 ^ 2 ^ 2 ^ ... 2 ^ 2; n 个 2
+(define (k n) (* 5 n n))  ; --> 5 * n^2
