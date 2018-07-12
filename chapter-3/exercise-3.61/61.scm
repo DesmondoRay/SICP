3cd9b810f4b6454bb904c663ffb8be97300b4d90
DesmondoRay <desmondorayy@gmail.com>
2018-07-12 21:06:25 +0800

    exercise 61, 62

---
+++ chapter-3/exercise-3.61/61.scm
@@ -0,0 +1,25 @@
+;;; 61.scm
+
+(load "../exercise-3.60/60.scm")
+(load "../common/stream-enumerate-interval.scm")
+
+
+(define (reciprocal-series s)
+  (cons-stream 1 
+			   (scale-stream 
+				(mul-series (stream-cdr s)
+							(reciprocal-series s))
+				-1)))
+
+;; test
+(define s (stream-enumerate-interval 1 10)) ; s为 1～10 的流
+(define x (reciprocal-series s))
+(define s-x (mul-series s x)) ; s-x 包含10个元素
+
+; 前9个数
+(display (stream-head s-x 9))
+; 结果: (1 0 0 0 0 0 0 0 0)
+; 最后一个数
+(stream-ref s-x 9)
+;Value: 0
+
---
+++ chapter-3/exercise-3.62/62.scm
@@ -0,0 +1,34 @@
+;;; 62.scm
+
+(load "../exercise-3.61/61.scm")
+
+(define (div-series s1 s2)
+  (if (= (stream-car s2) 0)
+	  (error "Divide by 0 -- DIV-SERIES" (stream-car s2))
+	  (mul-series s1
+				  (reciprocal-series s2))))
+
+;; test
+(define s (stream-enumerate-interval 1 10)) ; s为 1～10 的流
+(define ones (cons-stream 1 ones))
+(define x (div-series ones s)) ; x 包含10个元素
+
+(load "../common/display-stream.scm")
+; 1/x * x = 1
+(display-stream (mul-series x s))
+#| 输出:
+1
+1
+1
+1
+1
+1
+1
+1
+1
+1
+|#
+
+;; 正切函数的幂级数
+(define tane-series (div-series sine-series cosine-series))
+
