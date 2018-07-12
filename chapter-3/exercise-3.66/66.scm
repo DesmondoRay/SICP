bdf27703e9f1616a53d774a1b625979078071e49
DesmondoRay <desmondorayy@gmail.com>
2018-07-13 03:26:40 +0800

    update 66.scm

--- chapter-3/exercise-3.66/66.scm
+++ chapter-3/exercise-3.66/66.scm
@@ -1,12 +1,7 @@
 ;;; 66.scm
 
 (load "../common/integers.scm")
-
-(define (interleave s1 s2)
-  (if (stream-null? s1)
-	  s2
-	  (cons-stream (stream-car s1)
-				   (interleave s2 (stream-cdr s1)))))
+(load "../common/interleave.scm")
 
 (define (pairs s t)
   (cons-stream
