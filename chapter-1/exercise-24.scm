97de2bec8241169077243d8e7acd99bc67e2bb17
DesmondoRay <desmondorayy@gmail.com>
2018-07-14 03:41:02 +0800

    update exercise-24.scm

--- chapter-1/exercise-24.scm
+++ chapter-1/exercise-24.scm
@@ -19,18 +19,31 @@
         ((fermat-test n) (fast-prime? n (- times 1)))
         (else false)))
 
-(define (square a) (* a a))
+(define (prime? n) (fast-prime? n 10))
 
-;;; *************************
-(define (search-for-primes n counts start-time)
-  (if (= 0 counts)
-      (if (fast-prime? n counts)
-          (search-for-primes (+ n 1) (- counts 1) start-time)
-          (search-for-primes (+ n 1) counts start-time))))
 
-(search-for-primes 10000000 3 (runtime))
+;; test
+(load "common/report-time.scm")
+(search-for-primes (square 1000000000) 3 (runtime))
 (newline)
-(search-for-primes 100000000 3 (runtime))
+(search-for-primes (square 10000000000) 3 (runtime))
 (newline)
-(search-for-primes 1000000000 3 (runtime))
+(search-for-primes (square 100000000000) 3 (runtime))
 (newline)
+#| 输出:
+1000000000000000003
+1000000000000000009
+1000000000000000031
+time: 19.999999999999574
+
+10000000000000000000009
+10000000000000000000057
+10000000000000000000081
+time: 30.000000000001137
+
+100000000000000000000000067
+100000000000000000000000123
+100000000000000000000000127
+time: 50.00000000000071
+|#
+; 注： 数字太小不足以得出准确的时间
