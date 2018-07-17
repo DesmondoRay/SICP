75688cea07bd74859992fa961d9b708f420e0a80
DesmondoRay <desmondorayy@gmail.com>
2018-07-17 22:18:44 +0800

    add common file -- print.scm

---
+++ chapter-4/common/print.scm
@@ -0,0 +1,10 @@
+;;; print all results
+;;; 接收多个参数，依次打印每个参数
+
+(define (print . args)
+  (cond ((null? args)
+		 (newline)
+		 (display "Print done!"))
+		(else (newline)
+			  (display (car args))
+			  (apply print (cdr args)))))
