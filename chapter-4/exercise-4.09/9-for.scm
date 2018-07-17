52ef895f4b59c87935abbe047b962b9ed7233a2d
DesmondoRay <desmondorayy@gmail.com>
2018-07-17 22:19:20 +0800

    exercise 4.9

---
+++ chapter-4/exercise-4.09/9-for.scm
@@ -0,0 +1,125 @@
+;;; exercise 4.9
+;; for形式到迭代
+
+#|
+注：不考虑初始化和更新为多条语句到情况；
+   可以将多余的初始化语句放在for循环外部，将多余的更新语句放在for循环内部。
+for example:
+(for ((set! i 1) 
+	  (< i 5) 
+	  (set! i (+ 1 i)))
+	 ((set! a (+ a i))
+	  (set! b (* b i))))
+相当于C语言： 
+for (i = 1; i < 5; i++) {
+    a += i;
+    b *= i;
+}
+|#
+
+(load "../exercise-4.03/3.scm")
+
+(define (for? exp) (tagged-list exp 'for)) ; 使用数据导向的话，可省略
+
+(define (for-initialize exp) (car (cadr exp)))  ; 初始化：(set! i 0)
+
+(define (for-test exp) (cadr (cadr exp))) ; 测试语句：(< i 5)
+
+(define (for-update exp) (caddr (cadr exp))) ; 更新语句：(set! i (+ 1 i))
+
+(define (for-body exp) (caddr exp))
+
+#| test
+(define f '(for ((set! i 0) (< i 5) (set! i (+ 1 i)))
+				((set! a (+ a i))
+				 (set! b (* b i)))))
+
+(load "../common/print.scm")
+(print (for-initialize f)
+	   (for-test f)
+	   (for-update f)
+	   (for-body f))
+output:
+(set! i 0)
+(< i 5)
+(set! i (+ 1 i))
+((set! a (+ a i)) (set! b (* b i)))
+|#
+
+(define for-func-name 'for-iter)
+
+(define (make-application operator operands)
+  (cons operator operands))
+
+(define (make-definition func body)
+  (list 'define func body))
+
+(define (eval-for exp env)
+  (eval-in-4 (for->combination exp) env))
+
+(define (for->combination exp)
+  ; 生成for-proc: '(for-iter)，作为一个无参数到过程
+  (let ((for-proc (cons for-func-name '())))
+	(let ((define-body (make-if (for-test exp)
+								(sequence->exp
+								 (append (for-body exp)
+										 (list (for-update exp) for-proc)))
+								'true)))
+	  (sequence->exp
+	   (list
+		(for-initialize exp)  ; 初始化
+		(make-definition for-proc define-body)  ; 定义(for-iter)
+		(make-application for-func-name '()))))))  ; 执行(for-iter)
+
+;; 将eval-for加入元循环求值器
+(put 'op 'for eval-for)
+
+
+#|
+;; test(交互模式下)
+(driver-loop)
+
+;;; M-Eval input: 
+(define a 0)
+
+;;; M-Eval value: 
+ok
+
+;;; M-Eval input: 
+(define b 1)
+
+;;; M-Eval value: 
+ok
+
+;;; M-Eval input: 
+(define i 0)
+
+;;; M-Eval value: 
+ok
+
+;;; M-Eval input: 
+(for ((set! i 1) (< i 5) (set! i (+ 1 i)))
+	 ((set! a (+ a i))
+	  (set! b (* b i))))
+
+;;; M-Eval value: 
+#t
+
+;;; M-Eval input: 
+a
+
+;;; M-Eval value: 
+10
+
+;;; M-Eval input: 
+b
+
+;;; M-Eval value: 
+24
+
+;;; M-Eval input: 
+i
+
+;;; M-Eval value: 
+5
+|#
---
+++ chapter-4/exercise-4.09/9-while.scm
@@ -0,0 +1,85 @@
+;;; exercise 4.9
+;; while形式到迭代
+
+#|
+ for example:
+(while (> a 0)
+	   ((set! b (+ b a))
+	    (set! a (- a 1))))
+相当于C语言： 
+while (a > 0)
+    b += a--;
+|#
+
+
+(load "../exercise-4.03/3.scm")
+
+(define (while? exp) (tagged-list exp 'while)) ;使用数据导向的话，可省略
+
+(define (while-test exp) (cadr exp))
+
+(define (while-body exp) (caddr exp))
+
+(define while-func-name 'while-iter)
+
+(define (make-application operator operands)
+  (cons operator operands))
+
+(define (make-definition func body)
+  (list 'define func body))
+
+(define (eval-while exp env)
+  (eval-in-4 (while->combination exp) env))
+
+(define (while->combination exp)
+  ; 生成while-proc: '(while-iter)，作为一个无参数到过程
+  (let ((while-proc (cons while-func-name '())))
+	(let ((define-body (make-if (while-test exp)
+								(sequence->exp 
+								 (append (while-body exp) (list while-proc)))
+								'true)))
+	  (sequence->exp
+	   (list 
+		(make-definition while-proc define-body)  ; 定义(while-iter)
+		(make-application while-func-name '())))))) ; 执行(while-iter)
+
+;; 将eval-while加入元循环求值器
+(put 'op 'while eval-while)
+
+
+#|
+;; test(交互模式下)
+(driver-loop)
+
+;;; M-Eval input: 
+(define a 3)
+
+;;; M-Eval value: 
+ok
+
+;;; M-Eval input: 
+(define b 0)
+
+;;; M-Eval value: 
+ok
+
+;;; M-Eval input: 
+(while (> a 0)
+	   ((set! b (+ b a))
+		(set! a (- a 1))))
+
+;;; M-Eval value: 
+#t
+
+;;; M-Eval input: 
+a
+
+;;; M-Eval value: 
+0
+
+;;; M-Eval input: 
+b
+
+;;; M-Eval value: 
+6
+|#
