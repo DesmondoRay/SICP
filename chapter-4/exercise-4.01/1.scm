eefa3681037eefdb56f58b651dc528fd3ac40f09
DesmondoRay <desmondorayy@gmail.com>
2018-07-18 00:08:50 +0800

    update 1.scm

--- chapter-4/exercise-4.01/1.scm
+++ chapter-4/exercise-4.01/1.scm
@@ -5,13 +5,13 @@
   (if (no-operands? exps)
 	  '()
 	  (let ((first (eval (first-operand exps) env)))
-		(cons first
-			  (list-of-values (rest-operands exps) env)))))
+      (cons first
+            (list-of-values (rest-operands exps) env)))))
 
 ;; 从右到左求值
 (define (list-of-values exps env)
   (if (no-operands? exps)
 	  '()
 	  (let ((rest (list-of-values (rest-operands exps) env)))
-		(cons (eval (first-operand exps) env)
-			  rest))))
+      (cons (eval (first-operand exps) env)
+            rest))))
