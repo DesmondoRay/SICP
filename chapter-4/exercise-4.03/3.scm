fb0aea74013b87f422dc40e4bf469f887a1708d0
DesmondoRay <desmondorayy@gmail.com>
2018-07-18 00:09:07 +0800

    update 3.scm

--- chapter-4/exercise-4.03/3.scm
+++ chapter-4/exercise-4.03/3.scm
@@ -15,16 +15,16 @@
 
 (define (eval-in-4 exp env)
   (cond ((self-evaluating? exp) exp)
-		((variable? exp) (lookup-variable-value exp env))
-		; 字符串常量不需要env，也可以将quote放入数据导向表格内，但需要修改：
-		;    (define (text-of-quotation exp env) (cadr exp))
-		((quoted? exp) (text-of-quotation exp))
-		((get 'op (car exp)) ((get 'op (car exp)) exp env))
-		((application? exp)
-		 (apply-in-4 (eval-in-4 (operator exp) env)
-					 (list-of-values (operands exp) env)))
-		(else
-		 (error "Unknown expression type -- EVAL" exp))))
+        ((variable? exp) (lookup-variable-value exp env))
+        ; 字符串常量不需要env，也可以将quote放入数据导向表格内，但需要修改：
+        ;    (define (text-of-quotation exp env) (cadr exp))
+        ((quoted? exp) (text-of-quotation exp))
+        ((get 'op (car exp)) ((get 'op (car exp)) exp env))
+        ((application? exp)
+         (apply-in-4 (eval-in-4 (operator exp) env)
+                     (list-of-values (operands exp) env)))
+        (else
+         (error "Unknown expression type -- EVAL" exp))))
 
 
 ;; (driver-loop)
@@ -63,19 +63,25 @@ a
 5
 
 ;;; M-Eval input: 
-(if (= a 0) 0 (begin (display "positive") a))	; test: if? and begin?
+(if (= a 0) 
+    0 
+    (begin (display "positive") 
+           a))	; test: if? and begin?
 positive
 ;;; M-Eval value: 
 5
 
 ;;; M-Eval input: 
-(cond ((= a 0) 0) ((= a 3) 3) (else a))			; test: cond?
+(cond ((= a 0) 0) 
+      ((= a 3) 3) 
+      (else a))			; test: cond?
 
 ;;; M-Eval value: 
 5
 
 ;;; M-Eval input: 
-(define x (lambda (a b) (+ a b)))				; test: lambda?
+(define x (lambda (a b) 
+            (+ a b)))				; test: lambda?
 
 ;;; M-Eval value: 
 ok
