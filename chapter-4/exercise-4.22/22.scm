1c7d6656f8dac4a83d5234cb05dd6aca0330df94
DesmondoRay <desmondorayy@gmail.com>
2018-07-19 16:32:56 +0800

    exercise 4.22~4.24

---
+++ chapter-4/exercise-4.22/22.scm
@@ -0,0 +1,45 @@
+;;; exercise 4.22
+
+(load "../common/analyze-execute.scm")
+
+(define (let? exp) (tagged-list? exp 'let))
+
+(define (let-vars exp) (map car (cadr exp)))
+
+(define (let-vals exp) (map cadr (cadr exp)))
+
+(define (let-body exp) (cddr exp))
+
+(define (analyze-let exp)
+  (analyze (let->combination exp)))
+
+(define (make-application operator operands)
+  (cons operator operands))
+
+(define (let->combination exp)
+  (make-application (make-lambda (let-vars exp) (let-body exp))
+                    (let-vals exp)))
+
+
+#|
+;; test
+(driver-loop)
+;;; M-Eval input: 
+(define a 3)
+
+;;; M-Eval value: 
+ok
+
+;;; M-Eval input: 
+(define b 2)
+
+;;; M-Eval value: 
+ok
+
+;;; M-Eval input: 
+(let ((c (+ a 1)) (d (+ b 1)))
+(+ c d))
+
+;;; M-Eval value: 
+7
+|#
---
+++ chapter-4/exercise-4.24/24.scm
@@ -0,0 +1,64 @@
+;;; exercise 4.24
+
+;; test1
+; (load "../exercise-4.03/3.scm")
+
+;; test2
+(load "../exercise-4.22/22.scm")
+
+(define (driver-loop)
+  (prompt-for-input input-prompt)
+  (let ((input (read))
+        (start-time (runtime)))
+    (let ((output (eval-in-4 input the-global-environment)))
+      (announce-output output-prompt)
+      (display "runtime: ")
+      (display (- (runtime) start-time))
+      (newline)
+      (user-print output)))
+  (driver-loop))
+
+#|
+;; test1: 测试原版本的求值器
+(driver-loop)
+
+;;; M-Eval input: 
+(define (fib n)
+  (if (< n 2)
+      n
+      (+ (fib (- n 1)) (fib (- n 2)))))
+
+;;; M-Eval value: 
+runtime: 0.
+ok
+
+;;; M-Eval input: 
+(fib 22)
+
+;;; M-Eval value: 
+runtime: 6.37
+17711
+|#
+
+
+;; test2: 测试改进的求值器
+(driver-loop)
+
+;;; M-Eval input: 
+(define (fib n)
+  (if (< n 2)
+      n
+      (+ (fib (- n 1)) (fib (- n 2)))))
+
+;;; M-Eval value: 
+runtime: 0.
+ok
+
+;;; M-Eval input: 
+(fib 22)
+
+;;; M-Eval value: 
+runtime: 3.8400000000000003
+17711
+
+;; 可见，改进版本的运行时间从6.37秒减少到了3.84秒，所需时间约为原版本的60%左右。
